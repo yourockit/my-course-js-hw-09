f06cb9aa85ebf0969d7cee3ad1f7542ec9c1dbef
Yourockit <sts2ro@gmail.com>
2023-02-11 00:42:52 +0300

    my-js-hw-09-03

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,49 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const submit = document.querySelector('.form');
+
+submit.addEventListener('submit', onFormSubmit);
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
-}
+    const shouldResolve = Math.random() > 0.3;
+    return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                if (shouldResolve) {
+                    // Fulfill
+                    resolve({ position, delay });
+                } else {
+                    // Reject
+                    reject({ position, delay });
+                };
+            }, delay);
+        })
+        .then(({ position, delay }) => {
+            Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+        })
+        .catch(({ position, delay }) => {
+            Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+        });
+};
+
+function onFormSubmit(e) {
+    e.preventDefault();
+
+    let delay = Number(e.currentTarget.delay.value);
+    const step = Number(e.currentTarget.step.value);
+    const amount = Number(e.currentTarget.amount.value);
+
+    for (let position = 0; position < amount; position += 1) {
+        createPromise(position, delay)
+            // .then(({ position, delay }) => {
+            //     setTimeout(() => {
+            //         Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+            //     }, delay);
+            // })
+            // .catch(({ position, delay }) => {
+            //     setTimeout(() => {
+            //         Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+            //     }, delay);
+            // });
+        delay += step;
+    };
+};
